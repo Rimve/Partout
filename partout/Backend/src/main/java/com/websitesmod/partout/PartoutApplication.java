857b76335d02ecaf47446e1bdacf7973b102ae1a
Rimve <rimvydas.neverauskas@ktu.edu>
2020-09-19 14:34:57 +0300

    Added Spring

---
+++ partout/Backend/src/main/java/com/websitesmod/partout/PartoutApplication.java
@@ -0,0 +1,13 @@
+package com.websitesmod.partout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartoutApplication.class, args);
+	}
+
+}
---
+++ partout/Backend/src/test/java/com/websitesmod/partout/PartoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websitesmod.partout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
