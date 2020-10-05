c144e257f9c30ca57462829507bf556b30fb8d12
Rimve <rimvydas.neverauskas@ktu.edu>
2020-10-05 17:24:08 +0300

    Bearer

--- partout/Backend/src/main/java/com/websites/partout/filters/JwtRequestFilter.java
+++ partout/Backend/src/main/java/com/websites/partout/filters/JwtRequestFilter.java
@@ -32,7 +32,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         String username = null;
         String jwt = null;
 
-        if (authorizationHeader != null && authorizationHeader.startsWith("Token ")) {
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(6);
             username = jwtUtil.extractUsername(jwt);
         }
