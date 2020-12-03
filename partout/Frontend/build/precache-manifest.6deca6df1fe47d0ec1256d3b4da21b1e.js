7356e4b0d8863d5d494261cd1efd3f7fb868758c
Rimve <rimvydas.neverauskas@ktu.edu>
2020-12-03 02:41:39 +0200

    Push to deployment server

---
+++ partout/Frontend/build/precache-manifest.6deca6df1fe47d0ec1256d3b4da21b1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b85dff8b4a5db2aec5bbc867d7a935",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d946e8b6cf1c668e7ec9",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "3d81f800e9be5d9bf340",
+    "url": "/static/css/main.7cbab1ea.chunk.css"
+  },
+  {
+    "revision": "d946e8b6cf1c668e7ec9",
+    "url": "/static/js/2.20e025e8.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/static/js/2.20e025e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d81f800e9be5d9bf340",
+    "url": "/static/js/main.ffd7c8b4.chunk.js"
+  },
+  {
+    "revision": "977c230479d654b11ef1",
+    "url": "/static/js/runtime-main.b044721f.js"
+  }
+]);
